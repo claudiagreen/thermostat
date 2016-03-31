cbb4d98f34ad0069f637dbc432c784240dd79ae7
Claudia Green <claudia@claudiag.me.uk>
2016-03-31 10:49:32 +0100

    Added my own weather API

--- src/controller.js
+++ src/controller.js
@@ -28,7 +28,7 @@ $(document).ready(function() {
 
   function displayWeather(city) {
    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
-   var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+   var token = '&appid=19569d86978d1b271daecc3cb220ecd9';
    var units = '&units=metric';
    $.get(url + token + units, function(data) {
      $('#current-temperature').text(data.main.temp);
